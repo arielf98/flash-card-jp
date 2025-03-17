9b1aab10e26946cdd2ab32f16dac09c708ed9d4f
arielf98 <arielfebrian98@gmail.com>
2025-03-17 21:43:25 +0700

    basic flip flashcard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
+import FlashCard from './flash-cards'
 
 function App() {
 
+  const cards = [
+    { front: "What is React?", back: "A JavaScript library for UI." },
+    { front: "What is JSX?", back: "A syntax extension for JavaScript." },
+    { front: "What is Tailwind?", back: "A utility-first CSS framework." },
+  ];
+
   return (
-    <div  className='font-bold min-w-2xl border h-full p-1.5'>
+    <><p className='text-2xl font-bold mb-4'>
       JP Flash Cards
-    </div>
+    </p><div className='min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4'>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-3.5'>
+          {cards.map((card, index) => {
+            return (
+              <FlashCard key={index} front={card.front} back={card.back} />
+            );
+          })}
+        </div>
+      </div></>
   )
 }
 
--- src/flash-cards.tsx
+++ src/flash-cards.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react"
 
 type FlashCardT = {
-    front: 'string'
-    back: 'string'
+    front: string
+    back: string
 }
 
 export default function FlashCard(props: FlashCardT) {
     const [flipped, setFlipped] = useState(false)
 
     return (
-        <div onClick={() => setFlipped(state => !state)} className="w-64 h-40 items-center flex justify-center bg-white shadow-lg rounded-lg cursor-pointer transition-transform transform perspective-1000">
-            <div className={`relative w-full h-full flex items-center justify-center text-lg font-semibold text-gray-800 p-4 rounded-lg transition-transform duration-500 ease-in-out`}></div>
+        <div onClick={() => setFlipped(!flipped)} className="w-64 h-40 items-center flex justify-center bg-white shadow-lg rounded-lg cursor-pointer transition-transform transform perspective-1000">
+            <div className={`relative w-full h-full flex items-center justify-center text-lg font-semibold text-gray-800 p-4 rounded-lg transition-transform duration-500 ease-in-out ${flipped ? "rotate-y-180" : ""}`}
+                style={{ transformStyle: 'preserve-3d' }}>
+                {/* front */}
+                <div className="absolute w-full h-full flex items-center  justify-center bg-white shadow-lg rounded-lg backface-hidden">
+                    <p className="text-lg font-semibold text-gray-800">{props.front}</p>
+                </div>
+
+                {/* Back Side */}
+                <div className="absolute w-full h-full flex items-center justify-center bg-blue-500 text-white shadow-lg rounded-lg backface-hidden rotate-y-180">
+                    <p className="text-lg font-semibold">{props.back}</p>
+                </div>
+            </div>
         </div>
     )
 }
